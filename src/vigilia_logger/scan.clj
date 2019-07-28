a4614242066ebd1c382dbf4a0f4947bc676202ad
Frozenlock <frozenlock@gmail.com>
2019-07-28 12:15:46 -0400

    Print the time when a scan starts

--- src/vigilia_logger/scan.clj
+++ src/vigilia_logger/scan.clj
@@ -410,6 +410,7 @@
                     (mark-as-scanned! device-id)
                     scan-data))]
     ;; we begin a new scan
+    (println (str (encoding/iso-8601-timestamp) " - Scan network "))
     (mark-start-of-scan! ids-to-scan)
     (let [scan-result (doall (pmap scan-fn ids-to-scan))]
       (mark-end-of-scan!)
