cce6c88905a5fa4b20f24a4ca6091d0b03caa5c3
Frozenlock <frozenlock@gmail.com>
2023-01-03 10:58:09 -0500

    Version 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.hvac.vigilia/vigilia-logger "1.1.2"
+(defproject io.hvac.vigilia/vigilia-logger "1.2.0"
   :description "A library/app to record a BACnet network"
   :url "https://hvac.io"
   :license {:name "GNU GENERAL PUBLIC LICENSE"
